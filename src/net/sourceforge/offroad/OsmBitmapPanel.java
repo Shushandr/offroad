35ad7be501f47217269670d2dd834584fc703b8f
Christian Foltin <christianfoltin@users.sourceforge.net>
2016-04-04 19:39:14 +0200

    * antialiasing added.

--- src/net/sourceforge/offroad/OsmBitmapPanel.java
+++ src/net/sourceforge/offroad/OsmBitmapPanel.java
@@ -7,6 +7,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.RenderingHints.Key;
 import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.geom.AffineTransform;
@@ -160,6 +162,9 @@ public class OsmBitmapPanel extends JPanel {
 	private void drawImage(BufferedImage pImage) {
 		clear(pImage);
 		Graphics2D g2 = pImage.createGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		mWin.loadMGap(g2, getTileBox());
 		g2.dispose();
 	}
