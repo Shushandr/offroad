8f551d91c91b3a78fb7f59a3d5747a859ddfa1d3
Christian Foltin <christianfoltin@users.sourceforge.net>
2016-05-12 19:16:47 +0200

    * new group is possible

--- src/net/sourceforge/offroad/actions/AddFavoriteAction.java
+++ src/net/sourceforge/offroad/actions/AddFavoriteAction.java
@@ -1,27 +1,128 @@
 package net.sourceforge.offroad.actions;
 
+import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.Icon;
-import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import net.osmand.data.FavouritePoint;
 import net.osmand.data.LatLon;
+import net.osmand.plus.FavouritesDbHelper.FavoriteGroup;
 import net.sourceforge.offroad.OsmWindow;
+import net.sourceforge.offroad.ui.FavoriteGroupRenderer;
 
 public class AddFavoriteAction extends OffRoadAction {
+	private JTextField mNameTextField;
+	private JTextField mDescriptionTextField;
+	private JComboBox<FavoriteGroup> mComboBox;
+	private DefaultComboBoxModel<FavoriteGroup> mComboBoxModel;
+	private JCheckBox mNewGroupCheckBox;
+	private JTextField mGroupNameTextField;
+	private JColorChooser mGroupColorChooser;
+
 	public AddFavoriteAction(OsmWindow pContext, String pName, Icon pIcon) {
 		super(pContext, pName, pIcon);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent pE) {
-		String result = JOptionPane.showInputDialog("Enter favorite's name");
-		if(result != null){
-			LatLon pos = mContext.getCursorPosition();
-			mContext.getFavorites().addEmptyCategory("CategoryBla", 0x2288AA44);
-			mContext.getFavorites().addFavourite(new FavouritePoint(pos.getLatitude(), pos.getLongitude(), result, "CategoryBla"));
+		createDialog();
+		mDialog.setTitle(getResourceString("offroad.addFavorite"));
+		Container contentPane = mDialog.getContentPane();
+		GridBagLayout gbl = new GridBagLayout();
+		gbl.columnWeights = new double[] { 1.0f };
+		gbl.rowWeights = new double[] { 1.0f };
+		contentPane.setLayout(gbl);
+		int y = 0;
+		contentPane.add(new JLabel(getResourceString("offroad.favoriteName")), new GridBagConstraints(0, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		mNameTextField = new JTextField();
+		contentPane.add(mNameTextField, new GridBagConstraints(1, y++, 2, 1, 4.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		contentPane.add(new JLabel(getResourceString("offroad.favoriteDescription")), new GridBagConstraints(0, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		mDescriptionTextField = new JTextField();
+		contentPane.add(mDescriptionTextField, new GridBagConstraints(1, y++, 2, 1, 4.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		mComboBox = new JComboBox<FavoriteGroup>();
+		mComboBoxModel = new DefaultComboBoxModel<FavoriteGroup>();
+		for (FavoriteGroup filter : mContext.getFavorites().getFavoriteGroups()) {
+			mComboBoxModel.addElement(filter);
 		}
+		mComboBox.setModel(mComboBoxModel);
+		mComboBox.setFocusable(false);
+		mComboBox.setRenderer(new FavoriteGroupRenderer());
+		contentPane.add(mComboBox, new GridBagConstraints(1, y++, 2, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		contentPane.add(new JLabel(getResourceString("offroad.favoriteNewGroup")), new GridBagConstraints(0, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		mNewGroupCheckBox = new JCheckBox();
+		contentPane.add(mNewGroupCheckBox, new GridBagConstraints(1, y++, 2, 1, 4.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		mNewGroupCheckBox.addActionListener(t -> toggleNewGroup());
+
+		contentPane.add(new JLabel(getResourceString("offroad.favoriteGroupName")), new GridBagConstraints(0, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		mGroupNameTextField = new JTextField();
+		contentPane.add(mGroupNameTextField, new GridBagConstraints(1, y++, 2, 1, 4.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		mGroupNameTextField.setEnabled(false);
+
+		contentPane.add(new JLabel(getResourceString("offroad.favoriteGroupColor")), new GridBagConstraints(0, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		mGroupColorChooser = new JColorChooser();
+		contentPane.add(mGroupColorChooser, new GridBagConstraints(1, y++, 2, 1, 4.0, 1.0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+		mGroupColorChooser.setEnabled(false);
+		
+		
+		JButton okButton = new JButton(getResourceString("offroad.addFavoriteOK"));
+		contentPane.add(okButton, new GridBagConstraints(2, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		okButton.addActionListener(t -> terminate(true));
+		JButton cancelButton = new JButton(getResourceString("offroad.addFavoriteCancel"));
+		cancelButton.addActionListener(t -> terminate(false));
+		contentPane.add(cancelButton, new GridBagConstraints(1, y, 1, 1, 1.0, 1.0,
+				GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+		mDialog.pack();
+		mDialog.setVisible(true);
+
+	}
+
+	private void toggleNewGroup() {
+		boolean sel = mNewGroupCheckBox.isSelected();
+		mGroupNameTextField.setEnabled(sel);
+		mGroupColorChooser.setEnabled(sel);
+		mComboBox.setEnabled(!sel);
+	}
+
+	private void terminate(boolean pSaveResults) {
+		if (pSaveResults) {
+			// FIXME: Validierung1!
+			LatLon pos = mContext.getCursorPosition();
+			String groupName;
+			if (mNewGroupCheckBox.isSelected()) {
+				groupName = mGroupNameTextField.getText();
+				mContext.getFavorites().addEmptyCategory(groupName, mGroupColorChooser.getColor().getRGB());
+			} else {
+				groupName = mComboBoxModel.getElementAt(mComboBox.getSelectedIndex()).name;
+			}
+			FavouritePoint point = new FavouritePoint(pos.getLatitude(), pos.getLongitude(), mNameTextField.getText(),
+					groupName);
+			point.setDescription(mDescriptionTextField.getText());
+			mContext.getFavorites().addFavourite(point);
+			mContext.getDrawPanel().drawLater();
+		}
+		disposeDialog();
 	}
 
 	@Override
--- src/net/sourceforge/offroad/actions/OffRoadAction.java
+++ src/net/sourceforge/offroad/actions/OffRoadAction.java
@@ -93,7 +93,7 @@ public abstract class OffRoadAction extends AbstractAction {
 	public abstract void save();
 
 	protected String getResourceString(String pWindowTitle) {
-		return pWindowTitle;
+		return mContext.getOffRoadString(pWindowTitle);
 	}
 
 	public void createDialog() {
