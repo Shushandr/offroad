f79d65efecdb7be9d4a2ee3abecfc4e5f1653c23
Christian Foltin <christianfoltin@users.sourceforge.net>
2016-07-31 17:57:50 +0200

    * translations and gpx limitations added.

--- src/net/sourceforge/offroad/actions/ShowTrackDetailsAction.java
+++ src/net/sourceforge/offroad/actions/ShowTrackDetailsAction.java
@@ -33,6 +33,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.DateFormat;
+import java.text.MessageFormat;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -74,21 +75,32 @@ public class ShowTrackDetailsAction extends OffRoadAction {
 		contentDisplay.setContentType("text/html"); // let the text pane know this is what you want
 		String content = "<table border='2'><thead><th>Key</th><th>Value</th></thead><tbody>";
 		GPXTrackAnalysis analysis = mSelectedGpxFile.getGpxFile().getAnalysis(System.currentTimeMillis());
-		content += "<tr><td>" + mContext.getOffRoadString("startTime") + "</td><td>" + toString(analysis.startTime) + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("endTime") + "</td><td>" + toString(analysis.endTime) + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("timeSpan") + "</td><td>" + DateFormat.getTimeInstance().format(analysis.timeSpan) + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("timeMoving") + "</td><td>" + analysis.timeMoving + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("totalDistance") + "</td><td>" + analysis.totalDistance + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("totalDistanceMoving") + "</td><td>" + analysis.totalDistanceMoving + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("points") + "</td><td>" + analysis.points + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("avgSpeed") + "</td><td>" + analysis.avgSpeed + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("maxSpeed") + "</td><td>" + analysis.maxSpeed + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("avgElevation") + "</td><td>" + analysis.avgElevation + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("minElevation") + "</td><td>" + analysis.minElevation + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("maxElevation") + "</td><td>" + analysis.maxElevation + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("diffElevation") + "</td><td>" + (analysis.maxElevation-analysis.minElevation) + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("diffElevationUp") + "</td><td>" + analysis.diffElevationUp + "</td></tr>";
-		content += "<tr><td>" + mContext.getOffRoadString("diffElevationDown") + "</td><td>" + analysis.diffElevationDown + "</td></tr>";
+		String inBrk = "</td><td align='right'>";
+		if(analysis.isTimeSpecified()){
+			content += "<tr><td>" + mContext.getOffRoadString("startTime") + inBrk + toString(analysis.startTime) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("endTime") + inBrk + toString(analysis.endTime) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("timeSpan") + inBrk + toTimeString(analysis.timeSpan) + "</td></tr>";
+		}
+		if(analysis.isTimeMoving()){
+			content += "<tr><td>" + mContext.getOffRoadString("timeMoving") + inBrk + toTimeString(analysis.timeMoving) + "</td></tr>";
+		}
+		content += "<tr><td>" + mContext.getOffRoadString("totalDistance") + inBrk + toDist(analysis.totalDistance) + "</td></tr>";
+		if(analysis.isTimeMoving()){
+			content += "<tr><td>" + mContext.getOffRoadString("totalDistanceMoving") + inBrk + toDist(analysis.totalDistanceMoving) + "</td></tr>";
+		}
+		content += "<tr><td>" + mContext.getOffRoadString("points") + inBrk + analysis.points + "</td></tr>";
+		if(analysis.isSpeedSpecified()){
+			content += "<tr><td>" + mContext.getOffRoadString("avgSpeed") + inBrk + toVelocity(analysis.avgSpeed) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("maxSpeed") + inBrk + toVelocity(analysis.maxSpeed) + "</td></tr>";
+		}
+		if(analysis.isElevationSpecified()){
+			content += "<tr><td>" + mContext.getOffRoadString("minElevation") + inBrk + toDist(analysis.minElevation) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("maxElevation") + inBrk + toDist(analysis.maxElevation) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("diffElevation") + inBrk + (toDist(analysis.maxElevation-analysis.minElevation)) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("diffElevationUp") + inBrk + toDist(analysis.diffElevationUp) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("diffElevationDown") + inBrk + toDist(analysis.diffElevationDown) + "</td></tr>";
+			content += "<tr><td>" + mContext.getOffRoadString("avgElevation") + inBrk + toDist(analysis.avgElevation) + "</td></tr>";
+		}
 		content += "</tbody></table";
 		contentDisplay.setText(content ); // showing off
 		contentDisplay.setEditable(false); // as before
@@ -127,8 +139,19 @@ public class ShowTrackDetailsAction extends OffRoadAction {
 		
 	}
 
+	private String toVelocity(float pValue) {
+		return MessageFormat.format("{0,number,#.##} km/h", pValue);
+	}
+
+	private String toDist(double pValue) {
+		return MessageFormat.format("{0,number,#.##} m", pValue);
+	}
+
 	private String toString(long pStartTime) {
 		return DateFormat.getDateTimeInstance().format(pStartTime);
+	}
+	private String toTimeString(long pStartTime) {
+		return DateFormat.getTimeInstance().format(pStartTime);
 	}
 
 }
